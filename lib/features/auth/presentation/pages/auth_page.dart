7844e08621e7d255a6587b156d2fc9bb7d5e73cb
Karol Gajda <jestemdobrywniczym@gmail.com>
2024-10-31 17:10:38 +0100

    Add basic version of google services auth

--- lib/features/auth/presentation/pages/auth_page.dart
+++ lib/features/auth/presentation/pages/auth_page.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:eco_hero_mobile/common/injection/dependency_injection.dart';
 import 'package:eco_hero_mobile/common/util/button.dart';
 import 'package:eco_hero_mobile/common/util/color_util.dart';
@@ -5,6 +8,7 @@ import 'package:eco_hero_mobile/common/util/extensions/bloc_extension.dart';
 import 'package:eco_hero_mobile/features/user/presentation/blocs/current_user_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class AuthPage extends StatelessWidget {
@@ -12,6 +16,14 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const List<String> scopes = ['email', 'profile'];
+
+    GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId:
+          '1025551109866-tghkfutlmpjfvdhfio9hi88pnpgskqfr.apps.googleusercontent.com',
+      scopes: scopes,
+    );
+
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -28,10 +40,7 @@ class AuthPage extends StatelessWidget {
               SizedBox(height: 4.h),
               PrimaryButtonWidget(
                 onTap: () async {
-                  await get<CurrentUserBloc>().addAndWait(CurrentUserLoaded('opkarol11@wp.pl'));
-                  if (context.mounted) {
-                    context.push('/');
-                  }
+                  await loginWithGoogle(googleSignIn);
                 },
                 content: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -79,6 +88,13 @@ class AuthPage extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(height: 1.h),
+              SecondaryButtonWidget(
+                title: 'Zaloguj się przykładowymi danymi',
+                onTap: () async {
+                  await loginWithMockData(context);
+                },
+              ),
               SizedBox(height: 4.h),
               SizedBox(
                 width: 92.w,
@@ -96,5 +112,59 @@ class AuthPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> loginWithMockData(BuildContext context) async {
+    await get<CurrentUserBloc>()
+        .addAndWait(CurrentUserLoaded('opkarol11@wp.pl'));
+    if (context.mounted) {
+      context.push('/');
+    }
+  }
+
+  Future<void> loginWithGoogle(GoogleSignIn googleSignIn) async {
+    GoogleSignInAccount? account = await googleSignIn.signIn();
+    if (account != null) {
+      print('Account: ${account.toString()}');
+
+      GoogleSignInAuthentication auth = await account.authentication;
+      String? idToken = auth.idToken;
+      String? accessToken = auth.accessToken;
+
+      log(auth.accessToken ?? '');
+
+      Response response = await get<Dio>().get(
+        'https://ecohero.q1000q.me/api/v1/user/auth/google/callback',
+        data: {
+          'idToken': idToken,
+          'accessToken': accessToken,
+        },
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          followRedirects: true,
+          validateStatus: (status) {
+            return status! <= 500;
+          },
+        ),
+      );
+
+      print('Response Headers: ${response.headers}');
+
+      if (response.statusCode == 200) {
+        print('Response: ${response.data}');
+        // String jwtToken = response.data['token'];
+        // print('JWT Token: $jwtToken');
+
+      } else {
+        print('Error: ${response.data}');
+      }
+
+      log(response.data);
+      googleSignIn.signOut();
+    } else {
+      print('Google Sign-In failed');
+    }
   }
 }
